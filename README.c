0c8b7d36909fa0cc6ed1bb709757c1d0ad3b6f23
Shivam Kumar <43650929+Mshivamkr2@users.noreply.github.com>
2020-05-23 20:50:52 +0530

    Update README.c

--- README.c
+++ README.c
@@ -1,59 +1,89 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include<queue>
+#include<map>
+using std::vector;
+using std::priority_queue;
+using std::min;
+using std::map;
+long int m1[1000000];
 
-/*To find minimum currency of any denominations type*/
-#include<stdio.h>
-int main()
+void unions(int i,int j,int n)
 {
-int amt,i,j,s=0,n;
-    printf("Enter your amount please!/n");
-scanf("%d",&amt);
-printf("enter the no. of denominations\n");
-scanf("%d",&n);
-int d[n];
-printf("enter the denominations\n");
-for(i=0;i<n;i++)
-{
-    scanf("%d",&d[i]);
-}
-int temp;
-for(j=0;j<n;j++)
- {
-     for(i=0;i<n;i++)
-    {
-    if(d[i]>d[i+1])
-{
-        temp=d[i];
-        d[i]=d[i+1];
-        d[i+1]=temp;
-}
-  }
-  }
-printf("Printing denominations in ascending order\n");
-for(i=0;i<n;i++)
-{
- printf("%d\n",d[i]);
-}
-int ch[n];
-for(i=0;i<n;i++)
-{
-ch[i]=0;
-}
+    int a= m1[i];
+    int b= m1[j];
+    int x= min(a,b);
 
-for(i=n-1;i>=0;i--)
-{
-    while(s<amt && (amt-s)>=d[i])
+    for(int k=0;k<n;k++)
     {
-        s=s+d[i];
-        ch[i]++;
+        if(m1[k]==a || m1[k]==b)
+            m1[k]=x;
     }
-
-
 }
-    for(i=0;i<n;i++)
-    {
-        printf("the no denominations of %d is %d\n ",d[i],ch[i]);
+double minimum_distance(vector<int> x, vector<int> y) {
+  double result = 0.;
+  int i,j,index1,index2;
 
-    }
+  int cor1;
+  int cor2;
+  priority_queue <double, vector < double >, std::greater<double> > q1;
+ map<double,vector<int> > ma;
+ map<double,vector<int> > mb;
+  for(i=0;i<x.size();i++)
+  {
 
+      for(j=0;j<x.size();j++)
+      {
+          if(i!=j)
+          {
+          double pt=sqrt(((x[i]-x[j])*(x[i]-x[j]))+((y[i]-y[j])*(y[i]-y[j])));
+          q1.push(pt);
+          ma[pt].push_back(i);
+          mb[pt].push_back(j);
+
+          }
+
+      }
+  }
+   int n=x.size();
+   int edges=0;
+   int k=n+1;
+  while(edges!=n-1)
+  {
+      double pi= q1.top();
+      q1.pop();
+      int c1= ma[pi].back();
+      ma[pi].pop_back();
+      int c2 = mb[pi].back();
+      mb[pi].pop_back();
+
+      if(m1[c1]!=m1[c2])
+      {
+      unions(m1[c1],m1[c2],n);
+      result+=pi;
+      edges++;
+
+      }
+
+      else if(m1[c1]==m1[c2])
+      {
+          continue;
+      }
+
+  }
+  return result;
 }
 
-
+int main() {
+  size_t n;
+  std::cin >> n;
+  vector<int> x(n), y(n);
+  for (size_t i = 0; i < n; i++) {
+    std::cin >> x[i] >> y[i];
+    m1[i]=i;
+  }
+  std::cout << std::setprecision(10) << minimum_distance(x, y) << std::endl;
+}
